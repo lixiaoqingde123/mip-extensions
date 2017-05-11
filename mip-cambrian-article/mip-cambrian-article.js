8ffecd7a8b7a19a5beeae24de15552b422fbfff8
lixiaoqing01 <lixiaoqing01@baidu.com>
2017-05-11 14:11:04 +0800

    寒武纪文章落地页组件

--- mip-cambrian-article/mip-cambrian-article.js
+++ mip-cambrian-article/mip-cambrian-article.js
@@ -8,34 +8,29 @@ define(function (require) {
 
     var customElement = require('customElement').create();
     var templates = require('templates');
-    // viewer 窗口
-    var viewer = require('viewer');
-
 
     /**
      * 构造元素，只会运行一次
      */
     customElement.prototype.build = function () {
-        // 获取公帐号ID
-        var id = this.element.getAttribute('id');
-        id && viewer.sendMessage('cambrian-header', id);
-
         // 获取文章ID
         var aid = this.element.getAttribute('aid');
         var element = this.element;
         // 文章数据
         var data;
-        var url = '/officeplatform/message/articleselect?aid=' + aid;
+        var ARTICLE_AJAX = '/officeplatform/message/articleselect?aid=' + aid;
 
         // 获取数据
-        fetch(location.href).then(function (res) {
+        fetch(ARTICLE_AJAX).then(function (res) {
             return res.text();
         }).then(function (text) {
             data = JSON.parse(text).data;
-        });
-
-        templates.render(element, data).then(function (html) {
-            element.innerHTML = html;
+            // 文章主题内容，分为html和style两部分，把style塞到style标签里
+            document.getElementsByTagName('style')[0].insertAdjacentHTML('beforeend', data.content.style);
+            // 渲染html
+            templates.render(element, data).then(function (html) {
+                element.innerHTML = html;
+            });
         });
     };
 
